2694ce7af171afc81659b3a74fbb1d87d76b0398
Majid Mohammadian <majeedmohammadian@gmail.com>
2023-05-16 13:57:21 +0430

    change media service to use object data

--- src/Facades/MediaService.php
+++ src/Facades/MediaService.php
@@ -2,13 +2,12 @@
 
 namespace JobMetric\Media\Facades;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static mixed get(Model $model, string $key = null, string $locale = null)
- * @method static void store(Model $model, array $data = [])
- * @method static void delete(Model $model, string $locale = null)
+ * @method static \JobMetric\Media\Object\Common common()
+ * @method static \JobMetric\Media\Object\Category category()
+ * @method static \JobMetric\Media\Object\File file()
  *
  * @see \JobMetric\Media\MediaService
  */

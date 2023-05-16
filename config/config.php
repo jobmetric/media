51941879734c3d99c0e4a910e7d77e7694decdb1
Majid Mohammadian <majeedmohammadian@gmail.com>
2023-05-16 13:56:24 +0430

    change config file

--- config/config.php
+++ config/config.php
@@ -17,4 +17,29 @@ return [
 
     "cache_time" => env("MEDIA_CACHE_TIME", 0),
 
+    /*
+    |--------------------------------------------------------------------------
+    | Collections
+    |--------------------------------------------------------------------------
+    |
+    | Collection type
+    */
+
+    'collections' => [
+        'public' => [
+            // Select disk to storage data
+            'disk'              => env("MEDIA_COLLECTION_PUBLIC_DISK", env('FILESYSTEM_DISK', 'local')),
+
+            // If you want to avoid uploading duplicate files, disable this option
+            "duplicate_content" => env("MEDIA_COLLECTION_PUBLIC_DUPLICATE_CONTENT", false),
+        ],
+        'avatar' => [
+            // Select disk to storage data
+            'disk'              => env("MEDIA_COLLECTION_AVATAR_DISK", env('FILESYSTEM_DISK', 'local')),
+
+            // If you want to avoid uploading duplicate files, disable this option
+            "duplicate_content" => env("MEDIA_COLLECTION_AVATAR_DUPLICATE_CONTENT", true),
+        ],
+    ],
+
 ];

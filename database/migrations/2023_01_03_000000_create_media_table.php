a9283973ee270e7d8e5d6372082365977c4c0399
Majid Mohammadian <majeedmohammadian@gmail.com>
2023-05-16 13:56:36 +0430

    change migration file

--- database/migrations/2023_01_03_000000_create_media_table.php
+++ database/migrations/2023_01_03_000000_create_media_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use JobMetric\Media\Enums\MediaTypeEnum;
 
 return new class extends Migration {
     /**
@@ -12,18 +13,20 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::create('medias', function (Blueprint $table) {
+        Schema::create('media', function (Blueprint $table) {
             $table->id();
 
             $table->string('name')->index();
-            $table->string('disk');
-            $table->uuid()->unique();
 
-            $table->foreignId('parent_id')->constrained('medias')->restrictOnDelete()->cascadeOnUpdate();
+            $table->foreignId('parent_id')->nullable()->constrained('media')->restrictOnDelete()->cascadeOnUpdate();
             $table->enum('type', [
-                'c', // category
+                'c', // folder
                 'f'  // file
             ])->index();
+            /**
+             * value: folder, file
+             * use: @extends MediaTypeEnum
+             */
 
             $table->string('mime_type')->nullable()->index();
             $table->unsignedBigInteger('size')->default(0)->index();
@@ -31,25 +34,38 @@ return new class extends Migration {
              * if type=c -> size=0 else size>=0
              */
 
+            $table->string('content_id', 40)->nullable()->unique()->index();
+            /**
+             * sha1 content file unique
+             */
+
             $table->json('additional')->nullable();
             /**
-             * user_id
+             * user_id : 0 -> anonymous , x>0 -> user
              * responsive
              * set icon for folder
              */
 
+            $table->string('disk');
             $table->string('collection')->default('public')->index();
             /**
              * value: public, avatar, ...
+             */
+
+            $table->string('filename')->unique();
+            /**
+             * filename = uuid + . + extension
              */
 
             $table->softDeletes();
             $table->nullableTimestamps();
         });
 
-        Schema::create('media_path', function (Blueprint $table) {
-            $table->foreignId('media_id')->index()->constrained('medias')->cascadeOnDelete()->cascadeOnUpdate();
-            $table->foreignId('path_id')->index()->constrained('medias')->restrictOnDelete()->cascadeOnUpdate();
+        Schema::create('media_paths', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('media_id')->index()->constrained('media')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignId('path_id')->index()->constrained('media')->restrictOnDelete()->cascadeOnUpdate();
             $table->integer('level')->default(0);
 
             $table->unique([
@@ -58,6 +74,7 @@ return new class extends Migration {
             ], 'MEDIA_PATH_UNIQUE');
         });
 
+        // if media:type=f insert in media_relations
         Schema::create('media_relations', function (Blueprint $table) {
             $table->morphs('relatable');
             /**
@@ -65,7 +82,7 @@ return new class extends Migration {
              */
 
             $table->string('collection');
-            $table->foreignId('media_id')->index()->constrained('medias')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId('media_id')->nullable()->index()->constrained('media')->cascadeOnUpdate()->cascadeOnDelete();
 
             $table->unique([
                 'relatable_type',
@@ -83,7 +100,7 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('medias');
+        Schema::dropIfExists('media');
         Schema::dropIfExists('media_path');
         Schema::dropIfExists('media_relations');
     }

d2b597643833640612fb97e88de2c05ed0a9a30c
majid mohammadian <majeedmohammadian@gmail.com>
2024-07-05 19:04:10 +0330

    add base language

---
+++ lang/en/base.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base media Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during Media for
+    | various messages that we need to display to the user.
+    |
+    */
+
+    'validation' => [
+        'errors' => 'Validation errors occurred.',
+    ],
+
+    'messages' => [
+    ],
+
+    'exceptions' => [
+    ],
+
+];

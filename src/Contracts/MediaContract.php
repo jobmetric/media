b704a95b8f00bf63c3689d885095cbf2d7b8640d
majid mohammadian <majeedmohammadian@gmail.com>
2024-07-14 00:38:06 +0330

    add media contract

---
+++ src/Contracts/MediaContract.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace JobMetric\Translation\Contracts;
+
+interface MediaContract
+{
+    /**
+     * media allow collections.
+     *
+     * @return array
+     */
+    public function mediaAllowCollections(): array;
+}

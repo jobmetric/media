23d6ceace941bd96d14ebffc33baa400c037e4ec
majid mohammadian <majeedmohammadian@gmail.com>
2024-08-03 20:04:32 +0330

    add route file and complete upload route

---
+++ routes/route.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Support\Facades\Route;
+use JobMetric\Media\Http\Controllers\MediaController;
+
+/*
+|--------------------------------------------------------------------------
+| Laravel Media Routes
+|--------------------------------------------------------------------------
+|
+| All Route in Laravel Media package
+|
+*/
+
+// media
+Route::prefix('media')->name('media.')->namespace('JobMetric\Media\Http\Controllers')->group(function () {
+    Route::middleware([
+        SubstituteBindings::class
+    ])->group(function () {
+        Route::post('upload', [MediaController::class, 'upload'])->name('upload');
+    });
+});

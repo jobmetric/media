d2e8d8b3d21de606e748c02f25ca010e20a9c5ba
majid mohammadian <majeedmohammadian@gmail.com>
2024-08-03 23:33:01 +0330

    add CURRENT_TIMESTAMP const to created_at field in media relation table for use attach method

--- database/migrations/2023_01_03_000002_create_media_relations_table.php
+++ database/migrations/2023_01_03_000002_create_media_relations_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
@@ -23,7 +24,7 @@ return new class extends Migration {
 
             $table->string('collection')->index();
 
-            $table->dateTime('created_at')->index();
+            $table->dateTime('created_at')->index()->default(DB::raw('CURRENT_TIMESTAMP'));
 
             $table->unique([
                 'media_id',

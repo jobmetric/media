57db314a3faf22188784cb913d3b7834c6bb40d8
majid mohammadian <majeedmohammadian@gmail.com>
2024-08-03 20:14:37 +0330

    add upload request file

---
+++ src/Http/Requests/UploadRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace JobMetric\Media\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use JobMetric\Media\Rules\MediaCollectionExistRule;
+use JobMetric\Media\Rules\MediaExistRule;
+use JobMetric\Media\Rules\MediaMostFolderRule;
+
+class UploadRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'file' => 'required|file',
+            'parent_id' => [
+                'sometimes',
+                'nullable',
+                'integer',
+                new MediaExistRule,
+                new MediaMostFolderRule
+            ],
+            'collection' => [
+                'string',
+                new MediaCollectionExistRule
+            ],
+        ];
+    }
+}

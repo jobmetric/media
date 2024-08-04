aff8202c644cd3181f9a1f31d04faaa4c7f49bda
majid mohammadian <majeedmohammadian@gmail.com>
2024-08-04 22:49:10 +0330

    add has response trait in package core v1.11

--- src/Http/Controllers/Controller.php
+++ src/Http/Controllers/Controller.php
@@ -5,8 +5,9 @@ namespace JobMetric\Media\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use JobMetric\PackageCore\Controllers\HasResponse;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, ValidatesRequests;
+    use AuthorizesRequests, ValidatesRequests, HasResponse;
 }

0a9175800564bbed4817d49a708be3676c186ab8
majid mohammadian <majeedmohammadian@gmail.com>
2024-08-06 00:24:52 +0330

    update index route

--- routes/route.php
+++ routes/route.php
@@ -18,7 +18,7 @@ Route::prefix('media')->name('media.')->namespace('JobMetric\Media\Http\Controll
     Route::middleware([
         SubstituteBindings::class
     ])->group(function () {
-        Route::get('index', [MediaController::class, 'index'])->name('index');
+        Route::get('/', [MediaController::class, 'index'])->name('index');
         Route::post('new-folder', [MediaController::class, 'newFolder'])->name('new-folder');
         Route::post('upload', [MediaController::class, 'upload'])->name('upload');
         Route::get('download/{media}', [MediaController::class, 'download'])->name('download');

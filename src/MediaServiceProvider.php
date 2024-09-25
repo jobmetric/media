8773263486efd2341ef75ed1c9b019f7d53800c0
majidmohammadian <majeedmohammadian@gmail.com>
2024-09-25 21:30:44 +0330

    add pagination information to resource collection for add api_links to meta

--- src/MediaServiceProvider.php
+++ src/MediaServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace JobMetric\Media;
 
+use Illuminate\Http\Resources\Json\ResourceCollection;
 use Illuminate\Support\Facades\Blade;
 use JobMetric\PackageCore\Exceptions\MigrationFolderNotFoundException;
 use JobMetric\PackageCore\Exceptions\RegisterClassTypeNotFoundException;
@@ -44,5 +45,55 @@ class MediaServiceProvider extends PackageCoreServiceProvider
 
         // add alias for components
         Blade::component('media::components.file-manager', 'file-manager');
+
+        // add pagination information to resource collection for add api_links to meta
+        ResourceCollection::macro('paginationInformation', function ($request, $paginated, $default) {
+            $default['meta']['api_links'] = [];
+
+            $current_page = $default['meta']['current_page'];
+            $last_page = $default['meta']['last_page'];
+
+            if ($current_page > $last_page) {
+                $current_page = $default['meta']['current_page'] = $last_page;
+            }
+
+            if ($last_page === 1) {
+                return $default;
+            }
+
+            if ($current_page > 1) {
+                $default['meta']['api_links'][] = [
+                    'active' => false,
+                    'label' => 'previous',
+                    'page' => $current_page - 1,
+                ];
+            }
+
+            $arrayItems[] = $current_page - 2;
+            $arrayItems[] = $current_page - 1;
+            $arrayItems[] = $current_page;
+            $arrayItems[] = $current_page + 1;
+            $arrayItems[] = $current_page + 2;
+
+            foreach ($arrayItems as $arrayItem) {
+                if ($arrayItem > 0 && $arrayItem <= $last_page) {
+                    $default['meta']['api_links'][] = [
+                        'active' => $arrayItem === $current_page,
+                        'label' => $arrayItem,
+                        'page' => $arrayItem,
+                    ];
+                }
+            }
+
+            if ($current_page < $last_page) {
+                $default['meta']['api_links'][] = [
+                    'active' => false,
+                    'label' => 'next',
+                    'page' => $current_page + 1,
+                ];
+            }
+
+            return $default;
+        });
     }
 }
